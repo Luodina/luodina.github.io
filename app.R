c22e2bf5618d919ebcb5de2ae7239bf048f6eb87
Marta Rudina <marta.rudina@gmail.com>
2020-06-22 02:40:22 +0800

    Add files via upload
    added reactive function

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(DT)
 library(ggplot2)
 library(leaflet)
 
-source("test.R")
+source("global.R")
 
 library(tidyverse)
 library(gganimate)
@@ -23,8 +23,7 @@ ui <- dashboardPage(
             # First tab content
             tabItem(tabName = "dashboard",
                     fluidRow(
-                        box(width=12, plotOutput("bar1")),
-                        
+                        box(width=12, plotOutput("bar1"))
                     ),
                     fluidRow(
                         box(width=6, plotOutput("pie")),
@@ -39,24 +38,32 @@ ui <- dashboardPage(
             # Second tab content
             tabItem(tabName = "map",
                 fluidPage(
-                    leafletOutput("mymap"),
-
-                    selectizeInput("selected1",
-                                   "Select Item to Display",
-                                   unique(listings$neighbourhood)),
-
-                    selectizeInput("selected2",
-                                   "Select Item to Display",
-                                   unique(listings$room_type))
-                    #p(),
-                    #actionButton("recalc", "New points")
+                    fluidRow(
+                        box(width=12,height = 500, leafletOutput("mymap")),
+                        box(width=12, textOutput("text1")
+                    ),
+                    fluidRow(
+                        box(width=6, selectizeInput("selected1",
+                                                    "Select Item to Display",
+                                                    unique(listings$neighbourhood))
+                            ),
+                        box(width=6, sliderInput("sliderPrice", "Slider:", min(listings$price),max(listings$price),value =c(100, 500)))
+                    ),
+                    fluidRow(
+                        box(width=6,  selectizeInput("selected2",
+                                                     "Select Item to Display",
+                                                     unique(listings$room_type))
+                            )
+                        )
+                        
+                    )
                 )
             ),
             
             # Third tab content
             tabItem(tabName = "data",
                     fluidRow(
-                        box(width=3, checkboxGroupInput("show_vars", "Columns in diamonds to show:",
+                        box(width=3, checkboxGroupInput("show_vars", "AirBnb params:",
                                                         names(listings), selected = names(listings))),
                         box(width=9, dataTableOutput("table1"))    
                     )
@@ -68,9 +75,43 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+    listingHK <- reactive({
+        selNeighbourhood = input$selected1
+        selRoomType = input$selected2
+        startPrice = input$sliderPrice[1]
+        endPrice = input$sliderPrice[2]
+        listings %>%
+            select(neighbourhood,room_type, longitude,latitude,price) %>%
+            filter(neighbourhood == selNeighbourhood  
+                   & room_type == selRoomType
+                   & price >= startPrice
+                   & price <= endPrice)
+        
+    })
+    
+    mapListingsHk <- reactive({
+        num <- nrow(listingHK())
+        print(num)
+        if (num==0){
+            #default map - first 20 records
+            listings[1:20,]
+        }else {
+            listingHK()
+        }    
+    })
+    
     output$plot1 <- renderPlot({
         data <- histdata[seq_len(input$slider)]
         hist(data)
+    })
+    
+    output$text1 <-renderText({
+        num <- nrow(listingHK())
+        if (num==0){
+            "ERROR!!!"
+        }else {
+            "here we are" 
+        }  
     })
     
     output$bar1 <-renderPlot({
@@ -78,9 +119,7 @@ server <- function(input, output) {
     })
     
     output$mymap <- renderLeaflet({
-        leaflet(data = listings[listings$neighbourhood == input$selected1
-                                &listings$room_type == input$selected2,]
-                ) %>% addTiles() %>%
+        leaflet(data = mapListingsHk()) %>% addTiles() %>%
             addMarkers(~longitude, ~latitude, popup = ~as.character(room_type), label = ~as.character(price))
     })
     
